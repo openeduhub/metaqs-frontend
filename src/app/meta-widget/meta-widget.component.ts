82de7ad7c1e1af1e22301ca39b5f002ad32e4f08
Christopher Lübbemeier <luebbemeier@metaventis.com>
2022-04-06 17:49:37 +0200

    fix: inline icons

--- src/app/meta-widget/meta-widget.component.ts
+++ src/app/meta-widget/meta-widget.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { MetaWidgetService } from './meta-widget.service';
 import { Collection, CollectionMaterialsCount, LearningMaterial } from '../api';
+import { SvgIconsService } from '../core/svg-icons.service';
 
 export enum Mode {
     Score = 'Score',
@@ -29,7 +30,7 @@ export class MetaWidgetComponent implements OnInit, OnChanges {
     @Input() mode: Mode;
     @Input() ticket: string;
 
-    constructor(private widgetService: MetaWidgetService) {}
+    constructor(private widgetService: MetaWidgetService, private icons: SvgIconsService) {}
 
     getModeComponent() {
         if (this.mode === Mode.CollectionsTreeSearchCounts) {
@@ -46,6 +47,7 @@ export class MetaWidgetComponent implements OnInit, OnChanges {
 
     ngOnInit(): void {
         this.widgetService.init(this.collectionid, this.ticket);
+        this.icons.init();
     }
 
     ngOnChanges(changes: SimpleChanges): void {
